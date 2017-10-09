7da626591b29b2ce5187e2121a61d4d53bb7acca
Lightway <lightway29@gmail.com>
2017-10-09 17:38:08 +0530

    Single thread multiple event queue implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,64 +5,52 @@
 
 EventQueue _event_queue_one(32 * EVENTS_EVENT_SIZE, NULL);
 EventQueue _event_queue_two(32 * EVENTS_EVENT_SIZE, NULL);
-
 Thread _can_thread;
-Thread _sensor_thread;
+
+volatile bool sensor_interrupt_flag = 0;
 
 void event_queue_task() { printf("Event Task executed... \r\n"); }
+
+void event_queue_sensor_task() { printf("Event Sensor Task executed... \r\n"); }
 
 void event_queue_task_irq() {
   printf("Event added to queue... \r\n");
   _event_queue_one.call(&event_queue_task);
 }
 
-void event_queue_sensor_task() { printf("Event Sensor Task executed... \r\n"); }
-
 void event_queue_sensor_task_irq() {
   printf("Sensor Event added to queue... \r\n");
+  sensor_interrupt_flag = 1;
   _event_queue_two.call(&event_queue_sensor_task);
 }
 
-void can_task() {
-  printf("CAN Task... \r\n");
+void task_manager() {
   while (1) {
-    printf("Event dispatched... \r\n");
-    _event_queue_one.dispatch(100);
-    Thread::wait(100);
-  }
-}
-void sensor_task() {
-  printf("Sensor Task... \r\n");
-  while (1) {
-    printf("Sensor Event dispatched... \r\n");
-    _event_queue_two.dispatch(100);
+    if (sensor_interrupt_flag) {
+      _event_queue_one.break_dispatch();
+      _event_queue_two.dispatch(100);
+      sensor_interrupt_flag = 0;
+    } else {
+      _event_queue_one.dispatch(100);
+    }
+
     Thread::wait(100);
   }
 }
 
 int main() {
-  osStatus can_status = _can_thread.start(can_task);
+  osStatus can_status = _can_thread.start(task_manager);
   if (can_status != osOK) {
-    printf("[INFO] CAN thread not initialized \r\n");
+    printf("CAN thread not initialized \r\n");
   } else {
-    printf("[INFO] CAN thread initialized ... [OK]\r\n");
-  }
-
-  osStatus sensor_status = _sensor_thread.start(sensor_task);
-  if (sensor_status != osOK) {
-    printf("[INFO] Sensor thread not initialized \r\n");
-  } else {
-    printf("[INFO] Sensor thread initialized ... [OK]\r\n");
+    printf("CAN thread initialized ... [OK]\r\n");
   }
 
   Ticker canTicker;
-  canTicker.attach(&event_queue_task_irq, 2.0f);
-
-  Ticker sensorTicker;
-  sensorTicker.attach(&event_queue_sensor_task_irq, 10.0f);
+  canTicker.attach(&event_queue_task_irq, 3.0f);
 
   while (1) {
-    printf("In main loop. \r\n");
-    Thread::wait(100);
+    event_queue_task_irq();
+    Thread::wait(1000);
   }
 }
