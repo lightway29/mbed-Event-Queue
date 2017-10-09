33803bb117699271dc988fe6a6ca1dc1a62fc217
Lightway <lightway29@gmail.com>
2017-10-09 20:20:19 +0530

    issue fix

--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,54 @@ Thread _can_thread;
 
 volatile bool sensor_interrupt_flag = 0;
 
-void event_queue_task() { printf("Event Task executed... \r\n"); }
+void event_queue_task() { printf("Event Task executed\r\n"); }
 
-void event_queue_sensor_task() { printf("Event Sensor Task executed... \r\n"); }
+void event_queue_sensor_task() { printf("Event Sensor Task executed\r\n"); }
 
 void event_queue_task_irq() {
-  printf("Event added to queue... \r\n");
-  _event_queue_one.call(&event_queue_task);
+  printf("Event added to queue\r\n");
+  _event_queue_two.break_dispatch();
+  for (size_t i = 0; i < 20; i++) {
+    _event_queue_one.call(&event_queue_task);
+  }
 }
 
 void event_queue_sensor_task_irq() {
-  printf("Sensor Event added to queue... \r\n");
+  printf("Sensor Event added to queue\r\n");
   sensor_interrupt_flag = 1;
-  _event_queue_two.call(&event_queue_sensor_task);
+  _event_queue_one.break_dispatch();
+  for (size_t i = 0; i < 10; i++) {
+    _event_queue_two.call(&event_queue_sensor_task);
+  }
 }
 
 void task_manager() {
   while (1) {
     if (sensor_interrupt_flag) {
-      _event_queue_one.break_dispatch();
-      _event_queue_two.dispatch(100);
+      printf("Sensor queue dispatched\r\n");
       sensor_interrupt_flag = 0;
+      _event_queue_two.dispatch();
     } else {
-      _event_queue_one.dispatch(100);
+      printf("Queue dispatched\r\n");
+      _event_queue_one.dispatch();
     }
-
-    Thread::wait(100);
+    Thread::wait(10);
   }
 }
 
 int main() {
   osStatus can_status = _can_thread.start(task_manager);
   if (can_status != osOK) {
-    printf("CAN thread not initialized \r\n");
+    printf("CAN thread not initialized\r\n");
   } else {
     printf("CAN thread initialized ... [OK]\r\n");
   }
 
   Ticker canTicker;
-  canTicker.attach(&event_queue_task_irq, 3.0f);
+  canTicker.attach(&event_queue_sensor_task_irq, 1.0f);
 
   while (1) {
     event_queue_task_irq();
-    Thread::wait(1000);
+    Thread::wait(10);
   }
 }
